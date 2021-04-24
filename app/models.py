33176522a7eca3f20ab0c4d58ce0e52e86e1849d
pseudo-sm <saswathcommand@gmail.com>
2021-04-24 22:21:55 +0530

    mostly final changes

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+from pytz import utc
+
 
 class Alert(models.Model):
 
@@ -15,6 +17,21 @@ class Alert(models.Model):
 
     def __str__(self):
         return self.name + " - " + self.what
+
+    @property
+    def get_duration(self):
+        now = datetime.utcnow().replace(tzinfo=utc)
+        duration = now - self.datetime
+        print(self.datetime,now)
+        print(duration.days,duration.seconds)
+        if duration.days == 0:
+            if duration.seconds / 3600 < 1:
+                return str(int(duration.seconds / 60)) + " minutes"
+            elif duration.days < 1:
+                return str(int(duration.seconds / 3600)) + " hours"
+        else:
+            return str(int(duration.days)) + " days"
+
 
 class Enquiry(models.Model):
 
